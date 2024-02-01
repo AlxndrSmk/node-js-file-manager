b3f4063595db268d361ee8f983ba15e0cd197230
Alexander Samak <voprosy262@gmail.com>
2024-02-01 23:01:08 +0300

    feat: add leaving message

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import process from 'process';
 
 const args = process.argv.slice(2);
+let username;
 
 args.map((el) => {
   if (el.startsWith('--')) {
-    const username = el.split('=')[1];
+    username = el.split('=')[1];
     console.log(`Welcome to the File Manager, ${username}!`);
   }
+});
+
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+
+  if (input === '.exit') {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    process.exit();
+  }
+});
+
+process.on('SIGINT', () => {
+  console.log(`
+Thank you for using File Manager, ${username}, goodbye!`);
+
+  process.exit();
 });
